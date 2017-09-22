26d8d1bd3e2de4d7bc04e24e3988087ea68c8c92
Bridget Lane <bnlane@gannett.com>
2017-09-22 11:54:18 -0400

    add tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # effective-broccoli
 
+[![codecov](https://codecov.io/gh/bridgetlane/effective-broccoli/branch/master/graph/badge.svg)](
+
 Travis test repo
--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println(hello())
+}
+
+func hello() string {
+	return "Hello world!"
 }
